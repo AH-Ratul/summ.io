9f32118b35a5a8d8fbb3da8a0a4f489830d70541
AH-Ratul <ahratul21@gmail.com>
2025-11-08 23:28:17 +0600

    feat: user route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { userRouter } from "../modules/user/user.route";
 
 type TRoute = {
   path: string;
@@ -8,7 +9,10 @@ type TRoute = {
 const appRouter: Router = Router();
 
 const moduleRoutes: TRoute[] = [
-    
+  {
+    path: "/user",
+    route: userRouter,
+  },
 ];
 
 moduleRoutes.forEach((route) => appRouter.use(route.path, route.route));
