784e0aa1bc5100807744d14782096fa15f3cab11
AH-Ratul <ahratul21@gmail.com>
2025-11-09 16:57:19 +0600

    feat: wrap delete route with auth middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { UserController } from "./user.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../../../../generated/prisma";
 
 export const userRouter: Router = Router();
 
 userRouter.get("/", UserController.getUser);
 userRouter.post("/create", UserController.createUser);
-userRouter.delete("/:userId", UserController.deleteUser);
+userRouter.delete("/:userId", checkAuth(Role.ADMIN), UserController.deleteUser);
