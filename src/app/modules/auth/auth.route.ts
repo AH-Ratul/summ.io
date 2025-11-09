4a6246c3ead2793dc507430042bea3067fd25ee6
AH-Ratul <ahratul21@gmail.com>
2025-11-09 19:49:07 +0600

    feat: add change password route

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../../../../generated/prisma";
 
 export const authRoute: Router = Router();
 
 authRoute.post("/login", AuthController.credentialsLogin);
 authRoute.post("/logout", AuthController.logout);
+authRoute.post(
+  "/change-password",
+  checkAuth(...Object.values(Role)),
+  AuthController.changePassword
+);
