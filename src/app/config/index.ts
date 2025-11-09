95f12b40972c01d0e027208f20854d92ba1f5843
AH-Ratul <ahratul21@gmail.com>
2025-11-09 11:26:30 +0600

    feat: added jwt env variables

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,10 +6,24 @@ interface EnvConfig {
   PORT: string;
   NODE_ENV: "development" | "production";
   SALT: string;
+  JWT: {
+    ACCESS_SECRET: string;
+    ACCESS_EXPIRES: string;
+    REFRESH_SECRET: string;
+    REFRESH_EXPIRES: string;
+  };
 }
 
 const loadEnvironments = (): EnvConfig => {
-  const requiredVariables: string[] = ["PORT", "NODE_ENV", "SALT"];
+  const requiredVariables: string[] = [
+    "PORT",
+    "NODE_ENV",
+    "SALT",
+    "ACCESS_SECRET",
+    "ACCESS_EXPIRES",
+    "REFRESH_SECRET",
+    "REFRESH_EXPIRES",
+  ];
 
   requiredVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -21,6 +35,12 @@ const loadEnvironments = (): EnvConfig => {
     PORT: process.env.PORT as string,
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     SALT: process.env.SALT as string,
+    JWT: {
+      ACCESS_SECRET: process.env.ACCESS_SECRET as string,
+      ACCESS_EXPIRES: process.env.ACCESS_EXPIRES as string,
+      REFRESH_SECRET: process.env.REFRESH_SECRET as string,
+      REFRESH_EXPIRES: process.env.REFRESH_EXPIRES as string,
+    },
   };
 };
 
