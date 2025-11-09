d4bf120e317d03e88e3e7d324ac93b1892ddaf56
AH-Ratul <ahratul21@gmail.com>
2025-11-09 16:58:38 +0600

    add cookie-parser middleware

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { Application, Request, Response } from "express";
 import appRouter from "./app/routes";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFound } from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1", appRouter);
 
