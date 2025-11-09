3e8dd631b9a963bb22a6200906b95f98f7d2975d
AH-Ratul <ahratul21@gmail.com>
2025-11-09 11:24:41 +0600

    feat: add token create utility function

---
+++ src/app/utils/tokens.ts
@@ -0,0 +1,27 @@
+import { Prisma } from "../../../generated/prisma";
+import { envConfig } from "../config";
+import { generateToken } from "./jwt";
+
+export const createToken = (user: Partial<Prisma.UserWhereUniqueInput>) => {
+  const jwtPayload = {
+    id: user.id,
+    email: user.email,
+  };
+
+  const accessToken = generateToken(
+    jwtPayload as unknown as string,
+    envConfig.JWT.ACCESS_SECRET,
+    envConfig.JWT.ACCESS_EXPIRES
+  );
+
+  const refreshToken = generateToken(
+    jwtPayload as unknown as string,
+    envConfig.JWT.REFRESH_SECRET,
+    envConfig.JWT.REFRESH_EXPIRES
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
+};
