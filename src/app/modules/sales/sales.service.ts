be1c19c3b6d7b8725c5f0d0091797a7ab32932e4
AH-Ratul <ahratul21@gmail.com>
2025-11-25 20:22:37 +0600

    feat: handle sales creation logics

--- src/app/modules/sales/sales.service.ts
+++ src/app/modules/sales/sales.service.ts
@@ -1,9 +1,48 @@
 import { Prisma } from "../../../../generated/prisma";
 import { prisma } from "../../config/db";
+import AppError from "../../helpers/AppError";
 
-const createSales = async (payload: Prisma.SalesCreateInput) => {
+const createSales = async (payload: Prisma.SalesUncheckedCreateInput) => {
+  const product = await prisma.product.findUnique<Prisma.ProductFindUniqueArgs>(
+    {
+      where: {
+        id: payload.productId,
+      },
+    }
+  );
+
+  const qtyInNumber = Number(payload.quantity);
+  const productPrice = product?.price as number;
+
+  const total = qtyInNumber * productPrice;
+
+  if (qtyInNumber > product?.stock!) {
+    throw new AppError(
+      400,
+      `Insufficient stock. Only ${product?.stock} items available.`
+    );
+  }
+
+  const finalData = {
+    productId: payload.productId,
+    userId: payload.userId,
+    quantity: qtyInNumber,
+    total,
+  };
+
+  const updatedStock = product?.stock! - qtyInNumber;
+
   const sales = await prisma.sales.create({
-    data: payload,
+    data: finalData,
+  });
+
+  await prisma.product.update<Prisma.ProductUpdateArgs>({
+    where: {
+      id: product?.id,
+    },
+    data: {
+      stock: updatedStock,
+    },
   });
 
   return sales;
