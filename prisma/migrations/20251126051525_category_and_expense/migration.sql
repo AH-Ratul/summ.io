34982bced39daa12fd2697d528850305f1f59c35
AH-Ratul <ahratul21@gmail.com>
2025-11-26 11:35:07 +0600

    feat: add category & expense migration

---
+++ prisma/migrations/20251126051525_category_and_expense/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Expense" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Expense_pkey" PRIMARY KEY ("id")
+);
