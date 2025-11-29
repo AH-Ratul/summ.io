258e8d19cdb90eea05ead8c1e00e3d430c421223
AH-Ratul <ahratul21@gmail.com>
2025-11-29 20:05:35 +0600

    feat: new migration added

---
+++ prisma/migrations/20251129113414_category_relation/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `category` on the `Expense` table. All the data in the column will be lost.
+  - Added the required column `categoryId` to the `Expense` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Expense" DROP COLUMN "category",
+ADD COLUMN     "categoryId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Expense" ADD CONSTRAINT "Expense_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
