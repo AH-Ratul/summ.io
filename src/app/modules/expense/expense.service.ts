e6cf4119fd29365ce659cf5cee75aa07f1ee5ed2
AH-Ratul <ahratul21@gmail.com>
2026-01-01 15:40:36 +0600

    feat: implement filtering to expense retrieving

--- src/app/modules/expense/expense.service.ts
+++ src/app/modules/expense/expense.service.ts
@@ -1,3 +1,11 @@
+import {
+  endOfDay,
+  startOfDay,
+  startOfMonth,
+  startOfYear,
+  subDays,
+  subYears,
+} from "date-fns";
 import { Prisma } from "../../../../generated/prisma";
 import { prisma } from "../../config/db";
 import AppError from "../../helpers/AppError";
@@ -10,11 +18,86 @@ const addExpense = async (payload: Prisma.ExpenseUncheckedCreateInput) => {
   return expense;
 };
 
-const getExpense = async () => {
+const getExpense = async (query: Record<string, any>) => {
+  const page = Number(query.page) || 1;
+  const limit = Number(query.limit) || 10;
+  const range = query.range || "all-time";
+  const searchQ = query.search || "";
+  const cat = query.category || "";
+
+  // const targetYear = Number(query.year);
+  // const targetMonth = Number(query.month);
+
+  const filter: Prisma.ExpenseWhereInput = {};
+
+  if (cat && cat !== "all") {
+    filter.category = {
+      name: cat,
+    };
+  }
+
+  // if (targetYear && targetMonth) {
+  //   const startDate = new Date(
+  //     Date.UTC(targetYear, targetMonth - 1, 1, 0, 0, 0)
+  //   );
+  //   const endDate = new Date(Date.UTC(targetYear, targetMonth, 1, 0, 0, 0));
+
+  //   filter.date = {
+  //     gte: startDate,
+  //     lt: endDate,
+  //   };
+  // }
+
+  if (range) {
+    let startDate: Date | null = null;
+    let now = new Date();
+
+    switch (range) {
+      case "today":
+        startDate = startOfDay(now);
+        break;
+      case "last-7-days":
+        startDate = subDays(now, 7);
+        break;
+      case "last-15-days":
+        startDate = subDays(now, 15);
+        break;
+      case "last-30-days":
+        startDate = subDays(now, 30);
+        break;
+      case "this-month":
+        startDate = startOfMonth(now);
+        break;
+      case "this-year":
+        startDate = startOfYear(now);
+        break;
+      case "last-year":
+        startDate = subYears(now, 1);
+        break;
+      default:
+        startDate = null;
+    }
+
+    if (startDate) {
+      filter.date = {
+        gte: startDate,
+        lt: endOfDay(now),
+      };
+    }
+  }
+
+  filter.title = {
+    contains: searchQ,
+    mode: "insensitive",
+  };
+
   const expenses = await prisma.expense.findMany<Prisma.ExpenseFindManyArgs>({
+    where: filter,
     orderBy: {
       date: "desc",
     },
+    skip: (page - 1) * limit,
+    take: limit,
     include: {
       category: {
         select: {
@@ -24,7 +107,11 @@ const getExpense = async () => {
     },
   });
 
-  return expenses;
+  const totalCount = await prisma.expense.count();
+
+  const totalPage = Math.ceil(totalCount / limit);
+
+  return { page, limit, totalPage, expenses };
 };
 
 const updateExpense = async (
