4a3bd9195a1064021acd471e8264202376091a79
AH-Ratul <ahratul21@gmail.com>
2026-01-10 15:55:30 +0600

    feat: handle getMe controler & pass decoded token to getMe service

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { UserService } from "./user.service";
 import sendResponse from "../../utils/sendResponse";
+import { JwtPayload } from "jsonwebtoken";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.createUser(req.body);
@@ -16,6 +17,19 @@ const createUser = catchAsync(async (req: Request, res: Response) => {
 
 const getUser = catchAsync(async (req: Request, res: Response) => {
   const result = await UserService.getUser();
+
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "User Retrieved ",
+    data: result,
+  });
+});
+
+const getMe = catchAsync(async (req: Request, res: Response) => {
+  const decodedToken = req.user as JwtPayload;
+
+  const result = await UserService.getMe(decodedToken.id);
 
   sendResponse(res, {
     success: true,
@@ -41,5 +55,6 @@ const deleteUser = catchAsync(async (req: Request, res: Response) => {
 export const UserController = {
   createUser,
   getUser,
+  getMe,
   deleteUser,
 };
