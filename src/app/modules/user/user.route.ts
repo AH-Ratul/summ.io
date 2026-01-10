d934a47c2303fa3a5510afa39f6b9e2a3a5a9c79
AH-Ratul <ahratul21@gmail.com>
2026-01-10 15:56:02 +0600

    feat: add getMe route with authentication

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -6,5 +6,11 @@ import { Role } from "../../../../generated/prisma";
 export const userRouter: Router = Router();
 
 userRouter.get("/", UserController.getUser);
+userRouter.get("/me", checkAuth(...Object.values(Role)), UserController.getMe);
+
 userRouter.post("/register", UserController.createUser);
-userRouter.delete("/:userId", checkAuth(Role.ADMIN), UserController.deleteUser);
+userRouter.delete(
+  "/:userId",
+  checkAuth(Role.SUPER_ADMIN),
+  UserController.deleteUser
+);
